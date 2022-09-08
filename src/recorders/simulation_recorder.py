95fee5bde88301f54a36121d8f6f964411214e01
romanwixinger <roman.wixinger@gmail.com>
2022-09-08 11:34:23 +0900

    Created a recorder which uses simulated data.
    We can use it to stresstest the system with high number of new rows per second.

--- src/recorders/simulation_recorder.py
+++ src/recorders/simulation_recorder.py
@@ -15,21 +15,53 @@ sys.path.insert(0,'..')
 
 
 import pandas as pd
+import time
 
 from recorders.recorder import Recorder
 
 
 class SimulationRecorder(Recorder): 
+    """ Recorder which simulates reading a certain number of new lines each 
+        period from a csv with a certain number of columns. 
+    """
     
-    
-    def __init__(self, filepath: str):
-        super(SimulationRecorder, self).__init__(filepath, True)
+    def __init__(self, filepath: str, newlines_per_s: int, columns):
+        super(SimulationRecorder, self).__init__(filepath, False)
+        self.newlines_per_s = newlines_per_s
+        self.rows = 5 * self.newlines_per_s
+        self.columns = columns
+        self._setup_csv() 
+        self.last_load = time.time()
     
     def _load_new_data(self) -> pd.DataFrame:  
-        pass
+        time_diff = time.time() - self.last_load
+        self.last_load = time.time()        
+        nrows = int(time_diff * self.newlines_per_s)
+        nrows = min(nrows, self.rows)
+        nrows = max(nrows, 1)
+        df = pd.read_csv(self.filepath, nrows=nrows)
+        self.read_data_lines += len(df.index)
+        print(len(df.index))
+        return df
     
     def _load_metadata(self): 
-        pass
+        return None
     
     def _harmonize_time(self): 
-        pass
+        return
+    
+    def table_is_up_to_date(self) -> bool: 
+        return False
+    
+    def _setup_csv(self): 
+        """ Generates a big csv file on init, such that we can perform a real
+            read operation in the other methods. 
+        """
+        data=[i for i in range(self.rows)]
+    
+        dfs = [pd.DataFrame(data=data, columns=[f"col_{j}"]) for j in range(self.columns)]
+        dfs = dfs + [pd.DataFrame(data=[time.time() for i in range(self.rows)], columns=["timestamp"])]
+        df = pd.concat(dfs, axis=1)
+        df.to_csv(self.filepath)
+        return
+      
