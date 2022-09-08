5c3fc15492afbb6b238171e5d1849043fa723e39
romanwixinger <roman.wixinger@gmail.com>
2022-09-08 15:09:11 +0900

    Added constants for remote access.

--- src/constants.py
+++ src/constants.py
@@ -12,26 +12,26 @@ class Locations():
     
     @classmethod
     def __init__(self, 
-                 location: str,
-                 ssd: str,
-                 pmt: str,
-                 coil: str,
-                 heater: str,
-                 ion: str,
-                 gauge: str,
-                 laser: str,
-                 image: str,
+                 location: str="",
+                 ssd: str="",
+                 pmt: str="",
+                 coil: str="",
+                 heater: str="",
+                 ion: str="",
+                 gauge: str="",
+                 laser: str="",
+                 image: str="",
                  ): 
     
         self.location = "../data/"
-        self.ssd = location + ssd
-        self.pmt = location + pmt
-        self.coil = location + coil
-        self.heater = location + heater
-        self.ion = location + ion
-        self.gauge = location + gauge
-        self.laser = location + laser
-        self.image = location + image
+        self.ssd = ssd
+        self.pmt = pmt
+        self.coil = coil
+        self.heater = heater
+        self.ion = ion
+        self.gauge = gauge
+        self.laser = laser
+        self.image = image
         
                 
 """ Instances """
@@ -39,29 +39,33 @@ class Locations():
 # Romans settings
 roman_loc = Locations(
     location="../../data/sample/",
-    ssd="-20220314-100806-Slot1-In2.csv",
-    pmt="all_data.csv",
-    coil="coil_log.txt",
-    heater="HeaterLog_20220314_100740_00001.csv",
-    ion="IonBeamControl1.5_DESKTOP-8ICG2TJ_20220314_114132.csv",
-    gauge="TPG256GaugeMonitor_Single_DESKTOP-BEF5FI4_20220312_203214.csv",
-    laser="15.03.2022, 21.30, 384.22817013 THz.lta",
-    image="cmos_000039.csv"
+    ssd="../../data/sample/"+"-20220314-100806-Slot1-In2.csv",
+    pmt="../../data/sample/"+"all_data.csv",
+    coil="../../data/sample/"+"coil_log.txt",
+    heater="../../data/sample/"+"HeaterLog_20220314_100740_00001.csv",
+    ion="../../data/sample/"+"IonBeamControl1.5_DESKTOP-8ICG2TJ_20220314_114132.csv",
+    gauge="../../data/sample/"+"TPG256GaugeMonitor_Single_DESKTOP-BEF5FI4_20220312_203214.csv",
+    laser="../../data/sample/"+"15.03.2022, 21.30, 384.22817013 THz.lta",
+    image="../../data/sample/"+"cmos_000039.csv"
     )
 
 # Unittest settings
 unittest_loc = Locations(
     location="../../data/unittest/", 
-    ssd="ssd.csv",
-    pmt="pmt.csv", 
-    coil="coil.txt", 
-    heater="heater.csv",
-    ion="ion.csv", 
-    gauge="gauge.csv", 
-    laser="laser.lta", 
-    image="image.csv"
+    ssd="../../data/unittest/"+"ssd.csv",
+    pmt="../../data/unittest/"+"pmt.csv", 
+    coil="../../data/unittest/"+"coil.txt", 
+    heater="../../data/unittest/"+"heater.csv",
+    ion="../../data/unittest/"+"ion.csv", 
+    gauge="../../data/unittest/"+"gauge.csv", 
+    laser="../../data/unittest/"+"laser.lta", 
+    image="../../data/unittest/"+"image.csv"
     )
 
+# Remote settings
+remote_loc = Locations(
+    gauge="../../../TPG256Monitor/TPG256GaugeMonitor_Single_DESKTOP-BEF5FI4_20220908_124458.csv",
+    )
 
 # Standard setting
 loc = roman_loc
