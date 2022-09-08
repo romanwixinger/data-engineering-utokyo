4adad39975323aa06c474e62fb774ac06bb183df
romanwixinger <roman.wixinger@gmail.com>
2022-09-09 00:07:10 +0900

    Added unit tests for refreshing the get_table().

--- src/tests/test_recorders.py
+++ src/tests/test_recorders.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import sys
 sys.path.insert(0,'..')
 
 import unittest
 import pandas as pd
+import shutil
+import csv
 
 from constants import unittest_loc
 
@@ -37,12 +41,40 @@ paths = [
     unittest_loc.heater
     ]
 
+names = [
+    "ssd",
+    "pmt",
+    "coil",
+    "gauge",
+    "laser",
+    "ion",
+    "heater"
+    ]
+
 
 class Helper(): 
     
     @classmethod
     def get_nr_of_rows(cls, df: pd.DataFrame): 
         return len(df.index)
+
+    @classmethod
+    def try_except_wrapper(cls, func, message): 
+        try: 
+            func()
+        except Exception as e: 
+            message = message + f": {e}"
+            raise Exception(message)
+        return
+            
+    @classmethod
+    def _simulate_adding_rows(cls, recorder, n: int): 
+        """ Sets the counter back such that the reader reads the last part of
+            the csv again. 
+        """
+        recorder.read_data_lines = recorder.read_data_lines - n
+        recorder.last_updated = 0
+        return
 
 
 class TestRecorders(unittest.TestCase): 
@@ -52,28 +84,65 @@ class TestRecorders(unittest.TestCase):
             recorder = SpecialRecorder(filepath)
       
     def test_get_table(self): 
-        for SpecialRecorder, filepath in zip(recorders, paths): 
-            recorder = SpecialRecorder(filepath)
-            df = recorder.get_table()
-            assert(df is not None)
-            assert(Helper.get_nr_of_rows(df) == 5)
+        for SpecialRecorder, filepath, name in zip(recorders, paths, names): 
+             recorder = SpecialRecorder(filepath)
+             Helper.try_except_wrapper(
+                 func=recorder.get_table,
+                 message=f"test_get_table() failed for {SpecialRecorder}"
+            ) 
 
-    
+    def test_get_table_nr(self): 
+        for SpecialRecorder, filepath, name in zip(recorders, paths, names): 
+             recorder = SpecialRecorder(filepath)
+             df = recorder.get_table()
+             n = Helper.get_nr_of_rows(df) 
+             assert n == 6, f"test_get_table_nr() failed for {SpecialRecorder}: Found {n} instead of 6"
+
     def test_get_data(self): 
-        for SpecialRecorder, filepath in zip(recorders, paths): 
-            recorder = SpecialRecorder(filepath)
-            df = recorder.get_data()
-            assert(df is not None)
-            assert(Helper.get_nr_of_rows(df) == 5)
+        for SpecialRecorder, filepath, name in zip(recorders, paths, names): 
+             recorder = SpecialRecorder(filepath)
+             Helper.try_except_wrapper(
+                 func=recorder.get_data,
+                 message=f"test_get_data() failed for {SpecialRecorder}"
+            ) 
+    
+    def test_get_data_nr(self): 
+        for SpecialRecorder, filepath, name in zip(recorders, paths, names): 
+             recorder = SpecialRecorder(filepath)
+             df = recorder.get_data()
+             n = Helper.get_nr_of_rows(df) 
+             assert n == 6, f"test_get_data_nr() failed for {SpecialRecorder}: Found {n} instead of 6"
     
     def test_get_metadata(self): 
-        for SpecialRecorder, filepath in zip(recorders, paths): 
+        for SpecialRecorder, filepath, name in zip(recorders, paths, names): 
+             recorder = SpecialRecorder(filepath)
+             Helper.try_except_wrapper(
+                 func=recorder.get_metadata,
+                 message=f"test_get_metadata() failed for {SpecialRecorder}"
+            )
+        return
+        
+    def test_get_metadata_nr(self):  
+        for SpecialRecorder, filepath, name in zip(recorders, paths, names): 
             recorder = SpecialRecorder(filepath)
             df = recorder.get_metadata()
-            assert(df is not None)
-            assert(Helper.get_nr_of_rows(df) in [0,1])
-    
+            n = Helper.get_nr_of_rows(df)
+            assert n in [0,1], "test_get_metadata_nr() failed with n={n}."
+            
+    def test_refresh(self):
+        """ Test the case when the csv is modified and we load the new data.
+        """
+        
+        for SpecialRecorder, filepath, name in zip(recorders, paths, names):
+            set_back_n_rows = 6 if SpecialRecorder == LaserRecorder else 3
+            recorder = SpecialRecorder(filepath=filepath, always_update=True) 
+            n = Helper.get_nr_of_rows(recorder.get_table())
+            Helper._simulate_adding_rows(recorder, set_back_n_rows)
+            m = Helper.get_nr_of_rows(recorder.get_table())
+            assert m - n == (6/6 if SpecialRecorder == LaserRecorder else 3), f"test_refresh() with {SpecialRecorder} failed: n={n}, m={m}."
+        
     
 if __name__ == '__main__': 
+
     unittest.main()
     
