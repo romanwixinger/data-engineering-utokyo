[image] Fixed bug and added calculation.
Fixed a mixup between MOT number and power. Added more conversion factors, which can be used to convert the MOT number from the data. TODO: Finish conversion factors.

src/analyses/fit_mot_number.py
@@ -51,26 +51,10 @@ def get_scaling_factor(mode: str) -> float:
         mode, which is either 'power' or 'mot number'. 
     """
     scaling_factor = {
-        "power": 1.0 / c.Pow_elec_coef / c.MOTnum_Pow_coef, 
-        "mot number": c.hbar * c.omega0_Rb / (c.T_exp * c.eta)
+        "mot number": 1.0 / c.Pow_elec_coef / c.MOTnum_Pow_coef, 
+        "power": c.hbar * c.omega0_Rb / (c.T_exp * c.eta)
         }[mode]
     return scaling_factor
-
-def two_D_gauss(X: tuple, 
-                A: float, 
-                sigma_x: float, 
-                sigma_y: float, 
-                mu_x: float, 
-                mu_y: float, 
-                C: float): 
-    x, y = X 
-    z = A * c.Cell_xsize * c.Cell_ysize\
-        / (2*np.pi*np.sqrt(sigma_x**2*sigma_y**2))\
-        * np.exp(-(x-mu_x)**2/(2*sigma_x**2))\
-        * np.exp(-(y-mu_y)**2/(2*sigma_y**2))\
-        + C
-
-    return z
 
 def fitting(model: callable, data: dict, mode: str):
     """ Fit the model to the data."""
@@ -133,16 +117,16 @@ def get_initial_guess(data: dict, mode: str):
     """
     x, y = data["x"], data["y"] 
     
-    A = 6e5 if mode == "power" else 1e-6
+    A = 6e5 if mode == "mot number" else 1e-6
     mu_x = np.mean(x)
     mu_y = np.mean(y)
     sigma_x = np.std(x - mu_x * np.ones_like(x))
     sigma_y = np.std(y - mu_y * np.ones_like(y))
-    C = 1e2 if mode == "power" else 1e-12
+    C = 1e2 if mode == "mot number" else 1e-12
     p0 = np.array([A, sigma_x, sigma_y, mu_x, mu_y, C])
     return p0
     
-def generate_fit_data(data: dict, statistics: dict): 
+def generate_fit_data(model: callable, data: dict, statistics: dict): 
     """ Takes the x, y values of the data and the fit parameter, and returns
         fitted z values on a x, y grid in the same format as the data. 
     """
@@ -155,7 +139,7 @@ def generate_fit_data(data: dict, statistics: dict):
     X, Y = np.meshgrid(fit_x, fit_y)
     
     # Evaluate the fitted model on the grid
-    fit_z = two_D_gauss((X, Y), popt[0], popt[1], popt[2], popt[3], popt[4], popt[5])
+    fit_z = model((X, Y), popt[0], popt[1], popt[2], popt[3], popt[4], popt[5])
     # fit_z = two_D_gauss((X, Y), 200000., 0.0001, 0.0001, 0.00624, 0.0091, 102.)
     
     # Convert the fit into the same format as the data
@@ -188,13 +172,19 @@ def plot_fit_result(data: dict, fit_data: dict, target: str, mode: str):
     # Labels
     ax.set_xlabel('X (m)')
     ax.set_ylabel('Y (m)')
-    ax.set_zlabel('Power (W)' if mode=="power" else 'atom number')
+    ax.set_zlabel('Power (W)' if mode=="power" else 'Atom number [1/pixel]')
     
     # Save image
     plt.savefig(target, dpi=300)
     plt.show(block=False)
 
 def print_stats(statistics: dict):
+    if not statistics["fit_successful"]: 
+        print(" Result ***********")
+        print("Fit was not succesful.")
+        print("*******************")
+        return
+        
     print(" Result ***********")
     print("z = (A/(2*np.pi*sigma_x*sigma_y)) * np.exp(-(x-mu_x)**2/(2*sigma_x**2)) * np.exp(-(y-mu_y)**2/(2*sigma_y**2)) + C")
     print("A = ", statistics["A"], "+-", statistics["A_unc"])
@@ -217,10 +207,11 @@ def perform_analysis(source: str, target: str, mode: str):
     """
     df = load(source=source)
     data = preprocess(df, mode=mode)
-    statistics = fitting(model=two_D_gauss, data=data, mode=mode)
+    statistics = fitting(model=c.two_D_gauss, data=data, mode=mode)
     if statistics["fit_successful"]: 
-        fit_data = generate_fit_data(data, statistics)
+        fit_data = generate_fit_data(c.two_D_gauss, data, statistics)
         plot_fit_result(data, fit_data, target=target, mode=mode)
+    print_stats(statistics)
     return statistics
     
 

src/mot_constants.py
@@ -2,7 +2,8 @@
 """
 Created on Fri Sep  9 16:53:11 2022
 
-@author: roman
+@author: Shintaro Nagase (nagase@cns.s.u-tokyo.ac.jp) 
+@co-author: Roman Wixinger (roman.wixinger@gmail.com)
 
 MOT constants. 
 """
@@ -10,6 +11,7 @@ MOT constants.
 import numpy as np
 
 
+# Natural constants
 hbar= 1.054571817 * 10**(-34)     # Planck constant (Js)
 c = 299792458                     # Speed of light (m/s)
 
@@ -21,7 +23,7 @@ eta = 0.5                         # Quantum efficiency
 b = 10/5.3277                     # Lens magnification
 
 # Atom
-lambda_Rb = 780 * 10**(-9)        # Fluorescence wavelength (m)
+lambda_Rb = 780 * 10**(-9)          # Fluorescence wavelength (m)
 omega0_Rb = 2*np.pi*c/lambda_Rb
 Gamma_Rb = 2*np.pi * 7.6 * 10**(6)  # Life span (Hz)
 I_sat = 3.5771                      # Saturation strength (mW/cm^2)
@@ -44,9 +46,41 @@ VP_area = 8**2 * np.pi              # ICF34 viewport area (cm^2)
 r_VP = 65                           # Distance from MOT center to viewport (cm)
 Omega_VP = VP_area / r_VP**2        # Solid angle of viewport
 
-# Conversion formula
-Pow_elec_coef =  (T_exp*eta)/(hbar * omega0_Rb)     # Power (W) → Signal (electron) conversion 
-MOTnum_Pow_coef = hbar*omega0_Rb*Gamma_Rb/2 * s_0/(1+s_0) * 1/(1+(2*delta/Eff_Gamma_Rb)**2) * Omega_VP/(4*np.pi)    # MOT原子数→パワー(W)変換
+# Gaussian model
+def two_D_gauss(X: tuple, 
+                A: float, 
+                sigma_x: float, 
+                sigma_y: float, 
+                mu_x: float, 
+                mu_y: float, 
+                C: float): 
+    x, y = X 
+    z = A * Cell_xsize * Cell_ysize\
+        / (2*np.pi*np.sqrt(sigma_x**2*sigma_y**2))\
+        * np.exp(-(x-mu_x)**2/(2*sigma_x**2))\
+        * np.exp(-(y-mu_y)**2/(2*sigma_y**2))\
+        + C
+
+    return z
+
+# Conversion formula [CCD]
+Pow_elec_coef =  (T_exp*eta)/(hbar * omega0_Rb)  #  Power (W) → Signal (electron) conversion 
+MOTnum_Pow_coef = hbar*omega0_Rb*Gamma_Rb/2 * s_0/(1+s_0) * 1/(1+(2*delta/Eff_Gamma_Rb)**2) * Omega_VP/(4*np.pi)  # MOT Atomic Number to Power (W) Conversion
+Elec_MOTnum_coef = 1.0 / MOTnum_Pow_coef  / Pow_elec_coef #  Signal (electron) to MOT number conversion 
+
+# Current integrator
+pulse_per_coulomb = 10^-6   # (1/C)
+
+# PMT
+Gain = 1.0                  # (1)
+beta_cathode = 63.0 / 1000  # (A/W)
+
+# Conversion formula [PMT, Rubidium]
+Pow_pulses_per_s_coef = (1.0 / pulse_per_coulomb) * (1.0/(Gain * beta_cathode)) 
+MOTnum_pulses_per_s_coef = Pow_pulses_per_s_coef / MOTnum_Pow_coef 
+
+# Conversion formula [PMT, Francium]
+
 
 # Input data (observed data)
 Xmin = 480
@@ -55,3 +89,11 @@ Ymin = 630
 Ymax = 810
 Xnum = Xmax - Xmin
 Ynum = Ymax - Ymin
+
+# Functions
+
+def convert_pulse_rate_to_MOT(pulse_rate: float, with_beamsplitter: bool): 
+    """ Takes the pulse rate [1/s] of the PMT and whether or not a beamsplitter 
+        was applied. Returns the MOT number. 
+    """
+    return pulse_rate * MOTnum_pulses_per_s_coef * (2.0 if with_beamsplitter else 1.0)