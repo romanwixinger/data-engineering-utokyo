ea11d4a74d4f2e25995d9fa361103e2389ffe978
romanwixinger <roman.wixinger@gmail.com>
2022-09-17 13:54:23 +0900

    [recorder] Added ImageParser.
    The ImageParser just returns the new images. Refactored the ImageRecorder to make the methods represent what they do.

--- src/recorders/image_recorder.py
+++ src/recorders/image_recorder.py
@@ -35,9 +35,20 @@ class ImageRecorder(Recorder):
     def _load_initial_data(self) -> pd.DataFrame: 
         """ Returns all data (filepath and metadata of images) which are new. 
         """
+        return self._load_new_data()
+        
+    def _load_new_data(self) -> pd.DataFrame: 
+        """ Returns all data (filepath and metadata of images) which are new. 
+        """
+
         # Generate filepaths and add them to loaded
         filepaths = PathHelper.get_filepaths(folder=self.filepath, match=self.match)
         new_filepaths = set(filepaths) - self.filepath_set
+        
+        # Check if we have new filepaths (should always be the case!)
+        if len(new_filepaths) == 0: 
+            print("Do not call this function if there is no need for it!")
+            return pd.DataFrame()
         self.filepath_set = self.filepath_set | new_filepaths
         
         # Load metadata and create table
@@ -49,13 +60,7 @@ class ImageRecorder(Recorder):
                  os.path.getctime]
         rows = [list(func(path) for func in funcs) for path in new_filepaths]
         return pd.DataFrame(data=rows, columns=columns)
-        
-    def _load_new_data(self) -> pd.DataFrame: 
-        """ Returns all data (filepath and metadata of images) which are new. 
-        """
-
-        return self._load_initial_data()
-        
+    
     def _load_metadata(self) -> pd.DataFrame: 
         """ Reloads all metadata. 
         """
@@ -67,6 +72,22 @@ class ImageRecorder(Recorder):
         self._table_df["datetime_μs"] = self._table_df["datetime"]
         self._table_df["datetime_ms"] = self._table_df["datetime"].apply(lambda s: str(s)[:-3])
        
+        
+class ImageParser(ImageRecorder):
+    """ Like the ImageRecorder but on each evaluation of get_table(), 
+        the parser forgets the old data and just returns the new one. 
+    """
+    
+    def _update_data(self):
+        new_data_df = self._load_new_data()
+        self.read_data_lines += len(new_data_df.index)
+        self._data_df = new_data_df
+        self.last_updated = self._get_mod_time()
+        
+    def is_up_to_date(self) -> bool:
+        all_filepaths = PathHelper.get_filepaths(folder=self.filepath, match=self.match)
+        return len(self.filepath_set) == len(all_filepaths) 
+
     
 if __name__ == '__main__': 
     
@@ -74,7 +95,7 @@ if __name__ == '__main__':
     folder = "C:\\Users\\roman\\Desktop\\Research_UTokyo\\Data\\mot"
     match = ".*ccd_detuning.*.xlsx"
     
-    image_recorder = ImageRecorder(filepath=folder, match=match)
+    image_recorder = ImageParser(filepath=folder, match=match)
     df = image_recorder.get_table()
     for col in df.columns: 
         print(col, df[col])
