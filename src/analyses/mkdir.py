1e7e0204289552491d20a47a112c2ea92ccea473
romanwixinger <roman.wixinger@gmail.com>
2022-09-18 14:38:08 +0900

    [analysis] Initial commit of helper mkdir.

---
+++ src/analyses/mkdir.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 18 14:30:58 2022
+
+@author: Roman Wixinger (roman.wixinger@gmail.com)
+
+Test for creating folders. 
+"""
+
+import os
+
+def create_folders(plot_path: str, result_path: str): 
+    if not os.path.isdir(plot_path): 
+        os.mkdir(plot_path)
+        os.mkdir(plot_path + "ssd/")
+        os.mkdir(plot_path + "image/")
+        
+    if not os.path.isdir(result_path): 
+        os.mkdir(result_path)
