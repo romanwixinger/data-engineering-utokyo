834a3b0427e551161ee8dbb33ad5be4c90d3484e
romanwixinger <roman.wixinger@gmail.com>
2022-11-08 11:24:34 +0100

    [union] Added HeatTimeRecorder.
    The HeatTimeRecorder allows us to access the log files that were created for the Neutralizer heater.

---
+++ src/recorders/heat_time_recorder.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 8 13:31:02 2022
+
+@author: Roman Wixinger (roman.wixinger@gmail.com)
+
+Recorder for the HeatTimeLog. 
+"""
+
+
+import sys
+sys.path.insert(0,'../..')
+
+import numpy as np
+import pandas as pd
+
+from src.recorders.recorder import Recorder
+
+
+class HeatTimeRecorder(Recorder): 
+       
+    def __init__(self, filepath: str, day: str="2022-09-18-"):
+        super(HeatTimeRecorder, self).__init__(
+            filepath=filepath, 
+            has_metadata=False, 
+            delimiter="\t",
+            always_update=False
+            )
+        self.day = day
+        
+    def _load_initial_data(self) -> pd.DataFrame: 
+        """ Returns all data up to now and defines the data columns. 
+        """
+        return pd.read_csv(self.filepath, delimiter="\t", names=["Time", "VoltageDurationPower", "Coil"])
+        
+    def _load_new_data(self) -> pd.DataFrame: 
+        """ Returns the rows which have not been loaded so far. 
+        """
+        return
+    
+    def _harmonize_time(self): 
+        self._table_df["datetime"] = self._table_df["Time"].apply(lambda s: self.day + s).apply(pd.Timestamp)
+        self._table_df["timestamp"] = self._table_df["Time"].apply(pd.Timestamp).values.astype(np.int64)
+
+
+    
+    
+    
+    
